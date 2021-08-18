1d7fdee6c9976aa4f68e372e7b29034cbe788b83
Huu The <huuthe3vn@gmail.com>
2021-08-18 21:22:21 +0700

    Order grid total filter

--- modules/sale/middlewares/admin/orderGrid/[queryInit]totalColumn[grid].js
+++ modules/sale/middlewares/admin/orderGrid/[queryInit]totalColumn[grid].js
@@ -4,7 +4,20 @@ module.exports = (request, response, stack) => {
     // Handle filter
     if (request.query["grand_total"] !== undefined) {
         let query = stack["queryInit"];
-        query.andWhere("`order`.`grand_total`", "LIKE", `%${request.query["grand_total"]}%`);
-        assign(response.context, { grid: { currentFilter: { grand_total: request.query["grand_total"] } } });
+        if (/^\d+(\.\d+)?[-]\d+(\.\d+)?$/.test(request.query["grand_total"])) {
+            let ranges = request.query["grand_total"].split("-");
+            query.andWhere("`order`.`grand_total`", ">=", ranges[0]);
+            query.andWhere("`order`.`grand_total`", "<=", ranges[1]);
+            assign(response.context, { grid: { currentFilter: { grand_total: { from: ranges[0], to: ranges[1] } } } });
+        } else if (/^\d+(\.\d+)?[-]$/.test(request.query["grand_total"])) {
+            let ranges = request.query["grand_total"].split("-");
+            query.andWhere("`order`.`grand_total`", ">=", ranges[0]);
+            assign(response.context, { grid: { currentFilter: { grand_total: { from: ranges[0], to: undefined } } } });
+        } else if (/^[-]\d+(\.\d+)?$/.test(request.query["grand_total"])) {
+            let ranges = request.query["grand_total"].split("-");
+            query.andWhere("`order`.`grand_total`", "<=", ranges[1]);
+            assign(response.context, { grid: { currentFilter: { grand_total: { from: undefined, to: ranges[1] } } } });
+        } else
+            return
     }
 }
