26ec1acae0772c622d17d41c75fd5349f8ca41c2
Nguyen Huu The <thenguyen@Nguyens-MacBook-Pro.local>
2021-08-22 19:45:50 +0700

    Fix error product image loading

--- modules/catalog/middlewares/admin/productEdit/loadImages.js
+++ modules/catalog/middlewares/admin/productEdit/loadImages.js
@@ -1,21 +1,22 @@
 const { select } = require('@nodejscart/mysql-query-builder');
-const { pool } = require('../../../../../lib/mysql/connection');
+const { getConnection, pool } = require('../../../../../lib/mysql/connection');
 const { assign } = require("../../../../../lib/util/assign");
 const uniqid = require("uniqid");
 const { buildAdminUrl } = require("../../../../../lib/routie");
 
 module.exports = async (request, response) => {
+  let connection = await getConnection();
   let images = await select("image")
     .from("product_image")
     .where("product_image_product_id", "=", request.params.id)
-    .execute(pool);
+    .execute(connection);
 
   images = images.map((i) => { return { path: i.image, url: buildAdminUrl("adminStaticAsset", [i.image]), id: uniqid() } });
 
   let mainImage = await select("image")
     .from("product")
     .where("product_id", "=", request.params.id)
-    .load(pool);
+    .load(connection);
   if (mainImage["image"])
     images.unshift({ url: buildAdminUrl("adminStaticAsset", [mainImage["image"]]), path: mainImage["image"], id: uniqid() });
 
