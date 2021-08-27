684ab735e7732d165f5f06155fc459bd085975e8
The Nguyen <the.nguyen@macbooklocal.com>
2021-08-27 18:05:37 +0700

    Attribute name filter

--- modules/catalog/middlewares/admin/attributeGrid/[queryInit]nameColumn[grid].js
+++ modules/catalog/middlewares/admin/attributeGrid/[queryInit]nameColumn[grid].js
@@ -2,9 +2,9 @@ const { assign } = require("../../../../../lib/util/assign");
 
 module.exports = (request, response, stack) => {
     // Handle filter
-    if (request.query["name"] !== undefined) {
+    if (request.query["attribute_name"] !== undefined) {
         let query = stack["queryInit"];
-        query.andWhere("attribute.`name`", "LIKE", `%${request.query["name"]}%`);
-        assign(response.context, { grid: { currentFilter: { name: request.query["name"] } } });
+        query.andWhere("attribute.`attribute_name`", "LIKE", `%${request.query["attribute_name"]}%`);
+        assign(response.context, { grid: { currentFilter: { attribute_name: request.query["attribute_name"] } } });
     }
 }
