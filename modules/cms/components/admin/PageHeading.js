c441a1e9c22baa170962eb653fb426f4f716ae88
The Nguyen <the.nguyen@macbooklocal.com>
2021-08-29 23:42:43 +0700

    Heading space

--- modules/cms/components/admin/PageHeading.js
+++ modules/cms/components/admin/PageHeading.js
@@ -6,7 +6,7 @@ import { get } from '../../../../lib/util/get';
 const BreadcrumbIcon = ({ backUrl }) => {
     if (!backUrl)
         return null;
-    return <a href={backUrl} className='breadcrum-icon border block border-border rounded mr-125'>
+    return <a href={backUrl} className='breadcrum-icon border block border-border rounded mr-075'>
         <span className='flex items-center justify-center'><svg className='text-icon' viewBox="0 0 20 20" focusable="false" aria-hidden="true"><path d="M17 9H5.414l3.293-3.293a.999.999 0 1 0-1.414-1.414l-5 5a.999.999 0 0 0 0 1.414l5 5a.997.997 0 0 0 1.414 0 .999.999 0 0 0 0-1.414L5.414 11H17a1 1 0 1 0 0-2z"></path></svg></span>
     </a>;
 }
