27ca5c441997d0861657b4f118977e30450e24a5
The Nguyen <the.nguyen@macbooklocal.com>
2021-09-04 10:51:44 +0700

    Footer styling

--- modules/cms/components/admin/layout.js
+++ modules/cms/components/admin/layout.js
@@ -12,9 +12,8 @@ export default function AdminLayout() {
       </div>
       <div className="main-content">
         <Area id="content" className='main-content-inner' />
+        <div className="footer"><div className="copyright"><span>Copyright © 2021 Nodejscart Commerce</span></div><div className="version"><span>Version 1.0 dev</span></div></div>
       </div>
     </div>
-    <div className="footer"><div className="copyright"><span>Copyright © 2020 nodejscart Commerce</span></div><div className="version"><span>Version 1.0 dev</span></div></div>
-
   </>
 }
