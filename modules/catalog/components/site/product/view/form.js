e0eaa21d5c9703cca5ea0ac1ce30c2cced1d16ed
The Nguyen <the.nguyen@macbooklocal.com>
2021-09-20 21:14:56 +0700

    Add to cart button

--- modules/catalog/components/site/product/view/form.js
+++ modules/catalog/components/site/product/view/form.js
@@ -3,16 +3,24 @@ import { Form } from "../../../../../../lib/components/form/Form";
 import { Field } from "../../../../../../lib/components/form/Field";
 import { get } from "../../../../../../lib/util/get";
 import { useAppState, useAppDispatch } from "../../../../../../lib/context/app";
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../../../../../lib/components/form/Button";
 
-function AddToCart({ stockAvaibility }) {
+function AddToCart({ stockAvaibility, loading = false }) {
     return <div className="add-to-cart mt-2">
         <div style={{ width: '8rem' }}>
             <Field type='text' value='1' validationRules={['notEmpty']} className="qty" name={"qty"} placeholder={"Qty"} formId={"productForm"} />
         </div>
         <div className='mt-1'>
-            {stockAvaibility === 1 && <button className='button'>ADD TO CART</button>}
-            {stockAvaibility === 0 && <a className='button' href="#" onClick={(e) => e.preventDefault()}>SOLD OUT</a>}
+            {stockAvaibility === 1 && <Button
+                title="ADD TO CART"
+                outline={true}
+                isLoading={loading}
+                onAction={
+                    () => { document.getElementById('productForm').dispatchEvent(new Event('submit', { cancelable: true, bubbles: true })) }
+                }
+            />}
+            {stockAvaibility === 0 && <Button title="SOLD OUT" onAction={() => { }} />}
         </div>
     </div >
 }
@@ -21,12 +29,22 @@ export default function ProductForm({ action }) {
     const context = useAppState();
     const product = get(context, "product");
     const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(false);
 
     const onSuccess = (response) => {
         if (response.success === true)
             dispatch({ ...context, cart: response.data.cart });
     }
-    return <Form id={"productForm"} action={action} method={"POST"} submitBtn={false} onSuccess={onSuccess}>
+
+    return <Form
+        id={"productForm"}
+        action={action}
+        method={"POST"}
+        submitBtn={false}
+        onSuccess={onSuccess}
+        onStart={() => setLoading(true)}
+        onComplete={() => setLoading(false)}
+    >
         <input type="hidden" name="product_id" value={product.product_id} />
         <Area
             id="productSinglePageForm"
@@ -34,7 +52,8 @@ export default function ProductForm({ action }) {
                 {
                     'component': { default: AddToCart },
                     'props': {
-                        stockAvaibility: product.stock_availability
+                        stockAvaibility: product.stock_availability,
+                        loading: loading
                     },
                     'sortOrder': 50,
                     'id': 'productSingleBuyButton'
