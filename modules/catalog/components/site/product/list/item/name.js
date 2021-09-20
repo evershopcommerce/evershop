a8295031b10f1800159667acbbc3d579427967b7
The Nguyen <the.nguyen@macbooklocal.com>
2021-09-20 20:54:34 +0700

    Font size update

--- modules/catalog/components/site/product/list/item/name.js
+++ modules/catalog/components/site/product/list/item/name.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Name = ({ name, url, id }) => {
     return <div className="product-name product-list-name mt-1 mb-025">
-        <a href={url} className='font-bold hover:underline'><span>{name}</span></a>
+        <a href={url} className='font-bold hover:underline h5'><span>{name}</span></a>
     </div>
 };
 export { Name };
