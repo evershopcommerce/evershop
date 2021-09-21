30d54d79237ab2c9af56268d828b80f7e3eab396
The Nguyen <the.nguyen@macbooklocal.com>
2021-09-21 20:10:56 +0700

    Homepage

--- modules/cms/components/site/homepage/MainBanner.js
+++ modules/cms/components/site/homepage/MainBanner.js
@@ -5,7 +5,7 @@ export default function MainBanner() {
     return <div className='main-banner-home flex'>
         <div className='container text-center self-center'>
             <h2 className='h1 text-white'>Chic handbags reimagined for modern life.</h2>
-            <Button url="/products" title="Shop Now" variant="primary" />
+            <Button url="/products" title="SHOP NOW" variant="primary" />
         </div>
     </div>
 }
