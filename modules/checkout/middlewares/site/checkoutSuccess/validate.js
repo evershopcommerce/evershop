f9bb014f49922ce43ae7b4def3d9499b5447e296
The Nguyen <the.nguyen@macbooklocal.com>
2021-09-22 23:54:43 +0700

    Checkout success page part 2

--- modules/checkout/middlewares/site/checkoutSuccess/validate.js
+++ modules/checkout/middlewares/site/checkoutSuccess/validate.js
@@ -1,12 +1,10 @@
 const { buildSiteUrl } = require("../../../../../lib/routie");
 
-module.exports = async (request, response, stack, next) => {
-    // if (!request.session.orderId) {
-    //     response.redirect(302, buildSiteUrl("homepage"));
-    // } else {
-    //     // TODO: Load order
-    //     request.session.orderId = undefined;
-    //     next();
-    // }
-    next();
+module.exports = (request, response, stack, next) => {
+    if (!request.session.orderId) {
+        response.redirect(302, buildSiteUrl("homepage"));
+    } else {
+        // TODO: Load order
+        next();
+    }
 };
