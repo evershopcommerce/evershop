c7c63d54e9046dcc782bdb70a9a42e73f22dbe2f
The Nguyen <the.nguyen@macbooklocal.com>
2021-10-03 16:03:10 +0700

    Fix creating category

--- modules/catalog/middlewares/admin/categorySavePost/[getConnection]createCategory[finish].js
+++ modules/catalog/middlewares/admin/categorySavePost/[getConnection]createCategory[finish].js
@@ -1,12 +1,14 @@
 const { insert } = require('@nodejscart/mysql-query-builder');
+const { merge } = require('../../../../../lib/util/merge');
 
 module.exports = async (request, response, stack) => {
     if (request.body.category_id)
         return;
+    let data = merge(request.body, { include_in_nav: 0 })
     let connection = await stack["getConnection"];
-    let result = await insert("category").given(request.body).execute(connection);
+    let result = await insert("category").given(data).execute(connection);
     await insert("category_description")
-        .given(request.body)
+        .given(data)
         .prime("category_description_category_id", result.insertId).execute(connection);
 
     return result;
