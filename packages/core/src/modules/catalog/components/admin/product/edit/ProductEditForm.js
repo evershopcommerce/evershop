083751cfec0405a6be0769c7403b35a74aede9f0
The Nguyen <83757778+nodeonline@users.noreply.github.com>
2021-12-07 00:47:02 +0700

    Fix #13

--- packages/core/src/modules/catalog/components/admin/product/edit/ProductEditForm.js
+++ packages/core/src/modules/catalog/components/admin/product/edit/ProductEditForm.js
@@ -14,6 +14,9 @@ export default function ProductCreateForm(props) {
                 toast.error(get(response, "message", "Something wrong. Please reload the page!"));
             }
         }}
+        onError={() => {
+            toast.error("Something wrong. Please reload the page!");
+        }}
     >
         <div className="grid grid-cols-3 gap-x-2 grid-flow-row ">
             <div className="col-span-2 grid grid-cols-1 gap-2 auto-rows-max">
