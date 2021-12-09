cab1581f2dad7d8b4d117aa539d4741a454ec3ed
The Nguyen <>
2021-12-09 11:39:07 +0700

    Fix product edit loading forever

--- packages/core/src/modules/catalog/middlewares/admin/productEdit/loadImages.js
+++ packages/core/src/modules/catalog/middlewares/admin/productEdit/loadImages.js
@@ -1,23 +1,22 @@
 const { select } = require('@nodejscart/mysql-query-builder');
-const { getConnection, pool } = require('../../../../../lib/mysql/connection');
+const { pool } = require('../../../../../lib/mysql/connection');
 const { assign } = require("../../../../../lib/util/assign");
 const uniqid = require("uniqid");
 const { buildAdminUrl } = require("../../../../../lib/routie");
 
 module.exports = async (request, response) => {
   let productId = request.params.id;
-  let connection = await getConnection();
   let images = await select("image")
     .from("product_image")
     .where("product_image_product_id", "=", productId)
-    .execute(connection);
+    .execute(pool);
 
   images = images.map((i) => { return { path: i.image, url: buildAdminUrl("adminStaticAsset", [i.image]), id: uniqid() } });
 
   let mainImage = await select("image")
     .from("product")
     .where("product_id", "=", productId)
-    .load(connection);
+    .load(pool);
   if (mainImage["image"])
     images.unshift({ url: buildAdminUrl("adminStaticAsset", [mainImage["image"]]), path: mainImage["image"], id: uniqid() });
   assign(response.context, { product: { images: images } });
