dc1d295be5839bc5c1f2d0647c1a3b7a76bbd8a3
The Nguyen <>
2021-12-24 22:51:18 +0700

    Fix category saving error

--- packages/core/src/modules/catalog/components/admin/category/edit/CategoryEditForm.js
+++ packages/core/src/modules/catalog/components/admin/category/edit/CategoryEditForm.js
@@ -2,9 +2,22 @@ import React from "react";
 import Area from "../../../../../../lib/components/Area";
 import Button from "../../../../../../lib/components/form/Button";
 import { Form } from "../../../../../../lib/components/form/Form";
+import { toast } from "react-toastify";
+import { get } from "../../../../../../lib/util/get";
 
 export default function CategoryEditForm(props) {
-    return <Form {...props} submitBtn={false}>
+    return <Form
+        {...props}
+        onError={() => {
+            toast.error("Something wrong. Please reload the page!");
+        }}
+        onSuccess={(response) => {
+            if (get(response, "success") === false) {
+                toast.error(get(response, "message", "Something wrong. Please reload the page!"));
+            }
+        }}
+        submitBtn={false}
+    >
         <div className="grid grid-cols-3 gap-x-2 grid-flow-row ">
             <div className="col-span-2 grid grid-cols-1 gap-2 auto-rows-max">
                 <Area id="leftSide" noOuter={true} />
