56f7a9a2e3c1f5d16c2ca42ea5552e02e0175bf8
The Nguyen <>
2022-01-04 23:37:47 +0700

    Version alpha 30

--- packages/core/postpublish.js
+++ packages/core/postpublish.js
@@ -1,11 +1,9 @@
 var fs = require('fs')
 
-//fs.renameSync("./bin/dev/index.js", "./bin/dev/index_bk.js");
-
-const source = fs.readFileSync('./bin/dev/index.js', { encoding: 'utf8', flag: 'r' });
+const source = fs.readFileSync('./bin/serve/index.js', { encoding: 'utf8', flag: 'r' });
 
 var result = source.replace(/\.\.\/dist/g, '../src');
 
-fs.writeFile('./bin/dev/index.js', result, 'utf8', function (err) {
+fs.writeFile('./bin/serve/index.js', result, 'utf8', function (err) {
     if (err) return console.log(err);
 });
--- packages/core/prepublish.js
+++ packages/core/prepublish.js
@@ -2,11 +2,11 @@ var fs = require('fs')
 
 //fs.renameSync("./bin/dev/index.js", "./bin/dev/index_bk.js");
 
-const source = fs.readFileSync('./bin/dev/index.js', { encoding: 'utf8', flag: 'r' });
+const source = fs.readFileSync('./bin/serve/index.js', { encoding: 'utf8', flag: 'r' });
 
 var result = source.replace(/\.\.\/src/g, '../dist');
 
-fs.writeFile('./bin/dev/index.js', result, 'utf8', function (err) {
+fs.writeFile('./bin/serve/index.js', result, 'utf8', function (err) {
     if (err) return console.log(err);
 });
 
