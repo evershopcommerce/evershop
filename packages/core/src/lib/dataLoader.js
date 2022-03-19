e66ab166b50fc93e944aabb5bfbc852d0f88aed4
The Nguyen <>
2022-03-19 10:27:11 +0700

    Improve webpack code

--- packages/core/src/lib/dataLoader.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable quotes */
-/* eslint-disable global-require */
-export default function getDataLoader() {
-  return `const data = require('../webpack/pageData.json');module.exports = exports={};exports.getPageData = function getPageData(path) {return data[path]}`;
-}
