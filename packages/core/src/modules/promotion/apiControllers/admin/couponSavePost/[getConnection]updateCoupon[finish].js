1a03c9669043dc1e7359830c15b12a505e2e28cd
The Nguyen <huuthe3vn@gmail.com>
2022-06-01 11:37:20 +0700

    Create coupon API

---
+++ packages/core/src/modules/promotion/apiControllers/admin/couponSavePost/[getConnection]updateCoupon[finish].js
@@ -0,0 +1,15 @@
+const { update } = require('@evershop/mysql-query-builder');
+
+module.exports = async (request, response, stack) => {
+  if (!request.body.product_id) { return null; }
+
+  const connection = await stack.getConnection;
+  await update('product').given(request.body)
+    .where('product_id', '=', request.body.product_id)
+    .execute(connection);
+  await update('product_description')
+    .given(request.body).where('product_description_product_id', '=', request.body.product_id)
+    .execute(connection);
+
+  return request.body.product_id;
+};
