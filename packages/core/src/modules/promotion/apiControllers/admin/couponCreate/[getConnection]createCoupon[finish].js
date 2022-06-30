262d073e09e151fdceb237c49ba4cc925117afd5
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-06-30 23:01:05 +0700

    Fix coupon table name

--- packages/core/src/modules/promotion/apiControllers/admin/couponCreate/[getConnection]createCoupon[finish].js
+++ packages/core/src/modules/promotion/apiControllers/admin/couponCreate/[getConnection]createCoupon[finish].js
@@ -2,7 +2,7 @@ const { insert } = require('@evershop/mysql-query-builder');
 
 module.exports = async (request, response, stack) => {
   const connection = await stack.getConnection;
-  const result = await insert('coupon_json').given(request.body).execute(connection);
+  const result = await insert('coupon').given(request.body).execute(connection);
 
   return result;
 };
