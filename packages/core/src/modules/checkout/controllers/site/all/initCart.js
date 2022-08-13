5722131aca1f944660655a2cbf4a564c171b8a58
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-08-13 16:21:04 +0700

    Fix loosing cart items

--- packages/core/src/modules/checkout/controllers/site/all/initCart.js
+++ packages/core/src/modules/checkout/controllers/site/all/initCart.js
@@ -6,19 +6,19 @@ const { pool } = require('../../../../../lib/mysql/connection');
 module.exports = async (request, response, stack) => {
   let cart;
   // Check if there is a cart id in session
-  // if (request.session.cartId) {
-  //   // Check status of the cart
-  //   const c = await select().from('cart')
-  //     .where('cart_id', '=', request.session.cartId)
-  //     .and('status', '=', 1)
-  //     .load(pool);
-  //   if (c) {
-  //     cart = new Cart(request, { ...c });
-  //   } else {
-  //     // Remove cartId from session
-  //     request.session.cartId = undefined;
-  //   }
-  // }
+  if (request.session.cartId) {
+    // Check status of the cart
+    const c = await select().from('cart')
+      .where('cart_id', '=', request.session.cartId)
+      .and('status', '=', 1)
+      .load(pool);
+    if (c) {
+      cart = new Cart(request, { ...c });
+    } else {
+      // Remove cartId from session
+      request.session.cartId = undefined;
+    }
+  }
 
   // If there is no cart available, create a new cart object
   if (!cart) {
