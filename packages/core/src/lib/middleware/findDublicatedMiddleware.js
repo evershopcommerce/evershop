bbcaab82ce85d2672e224b6e41721bd591b01e7a
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-08-26 09:42:51 +0700

    lc

---
+++ packages/core/src/lib/middleware/findDublicatedMiddleware.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-multi-assign
+module.exports = exports = {};
+
+exports.findDublicatedMiddleware = function findDublicatedMiddleware(registeredMiddlewares, newMiddleware) {
+  return registeredMiddlewares.findIndex(
+    (middleware) => middleware.id === newMiddleware.id
+      && (
+        middleware.routeId === null
+        || newMiddleware.routeId === null
+        || middleware.routeId === newMiddleware.routeId
+        || (middleware.scope === newMiddleware.scope && (
+          [middleware.routeId, newMiddleware.routeId].includes('admin')
+          || [middleware.routeId, newMiddleware.routeId].includes('site')
+        )
+        )
+      )
+  );
+};
