d1b724965f82d68a886a07bc9550e036fe67a1ef
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-03 17:52:44 +0700

    GraphQL implementation

--- packages/core/src/lib/components/react/client/Hydrate.js
+++ packages/core/src/lib/components/react/client/Hydrate.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { AppProvider } from '../../../context/app';
 import { Alert } from '../../modal/Alert';
 import Area from '../../Area';
+import { createClient, Provider } from 'urql';
+
+const client = createClient({
+  url: '/v1/graphql'
+});
 
 export default function Hydrate() {
-  return <AppProvider value={window.eContext}>
-    <Alert>
-      <Area id="body" className="wrapper" />
-    </Alert>
-  </AppProvider>
+  return <Provider value={client}>
+    <AppProvider value={window.eContext}>
+      <Alert>
+        <Area id="body" className="wrapper" />
+      </Alert>
+    </AppProvider>
+  </Provider>
 };
