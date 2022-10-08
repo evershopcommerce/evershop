897cac56649728edbdb39a043f6ee6ca9833dc3d
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-08 17:48:02 +0700

    Fix eslint

--- packages/core/src/modules/auth/components/AuthContext.js
+++ packages/core/src/modules/auth/components/AuthContext.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react';
 import { createClient, Provider } from 'urql';
-import Area from "../../../lib/components/Area";
+import Area from '../../../lib/components/Area';
 import { useAppState } from "../../../lib/context/app";
 import { get } from "../../../lib/util/get";
 
