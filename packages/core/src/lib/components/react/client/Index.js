80789844f8177f88896aae441b56450b88c37f18
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-08 17:40:03 +0700

    Fix eslint

--- packages/core/src/lib/components/react/client/Index.js
+++ packages/core/src/lib/components/react/client/Index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Area from '@evershop/core/src/lib/components/Area';
-import { App } from "@evershop/core/src/lib/components/react/client/Client";
+import { App } from '@evershop/core/src/lib/components/react/client/Client';
 const hot = require('webpack-hot-middleware/client?path=/eHot&reload=true&overlay=true');
 import { HotReload } from '@evershop/core/src/lib/components/react/client/HotReload';
 /**render*/
