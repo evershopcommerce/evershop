4a323eb1b4b8f71201c02ab174430b6b433ac8c4
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-08 22:34:02 +0700

    Fix eslint

--- packages/core/src/modules/promotion/components/BuyXGetY.js
+++ packages/core/src/modules/promotion/components/BuyXGetY.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Field } from "../../../lib/components/form/Field";
 import { FORM_FIELD_UPDATED } from "../../../lib/util/events";
