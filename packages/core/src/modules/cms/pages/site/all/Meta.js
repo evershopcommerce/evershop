5b7105a28158cda39afb85a2adb091a31065d524
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-08 22:17:02 +0700

    Fix eslint

--- packages/core/src/modules/cms/pages/site/all/Meta.js
+++ packages/core/src/modules/cms/pages/site/all/Meta.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import React from 'react';
 import Meta from '../../../../../lib/components/Meta';
 import Title from '../../../../../lib/components/Title';
