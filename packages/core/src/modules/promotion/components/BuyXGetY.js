932c26f50fcb6a8fee4fbb6911b609af4c0dfd5e
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-08 22:36:02 +0700

    Fix eslint

--- packages/core/src/modules/promotion/components/BuyXGetY.js
+++ packages/core/src/modules/promotion/components/BuyXGetY.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Field } from '../../../lib/components/form/Field';
-import { FORM_FIELD_UPDATED } from "../../../lib/util/events";
+import { FORM_FIELD_UPDATED } from '../../../lib/util/events';
 
 export function BuyXGetY({ requireProducts, discountType }) {
   const [products, setProducts] = React.useState(requireProducts);
