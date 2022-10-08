75e89f34045bc94872f68e4c54ed5a9d19e5d1d2
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-08 19:39:03 +0700

    Fix eslint

--- packages/core/src/modules/promotion/pages/site/cart/Coupon.js
+++ packages/core/src/modules/promotion/pages/site/cart/Coupon.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import React from 'react';
 import { toast } from "react-toastify";
 import { get } from '../../../../../lib/util/get';
