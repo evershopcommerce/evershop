bc48700064c481d7b9ec568c38e843495bc46d81
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-09 10:55:02 +0700

    Fix eslint

--- packages/core/src/modules/catalog/pages/admin/attributeEdit+attributeNew/AttributeEditForm.js
+++ packages/core/src/modules/catalog/pages/admin/attributeEdit+attributeNew/AttributeEditForm.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import React from 'react';
 import Area from '../../../../../lib/components/Area';
 import Button from '../../../../../lib/components/form/Button';
