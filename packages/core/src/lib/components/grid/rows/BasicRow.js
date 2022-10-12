bbec2dff875fa793924812a475bda084debb1a3d
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-12 11:30:02 +0700

    Fix eslint

--- packages/core/src/lib/components/grid/rows/BasicRow.js
+++ packages/core/src/lib/components/grid/rows/BasicRow.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from "react";
+import React from 'react';
 
 export default function BasicRow({ id, areaProps }) {
   return <td>{areaProps.row[id]}</td>;
