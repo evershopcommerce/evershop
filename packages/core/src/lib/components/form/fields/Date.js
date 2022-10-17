9c5aace4759bf01bae07c7a9177bb12727bf6b85
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-17 14:30:02 +0700

    Fix eslint

--- packages/core/src/lib/components/form/fields/Date.js
+++ packages/core/src/lib/components/form/fields/Date.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from "react";
+import React from 'react';
 import flatpickr from './Flatpickr';
 import Error from './Error';
 import './../Field.scss';
