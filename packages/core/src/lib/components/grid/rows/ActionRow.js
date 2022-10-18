e1c10327d28c8e1512c8d0f9e43cb5a2bc978252
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-10-18 09:30:02 +0700

    Fix eslint

--- packages/core/src/lib/components/grid/rows/ActionRow.js
+++ packages/core/src/lib/components/grid/rows/ActionRow.js
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from "react";
+import React from 'react';
 import PropTypes from "prop-types";
 import { get } from '../../../util/get';
 
