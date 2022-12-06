823256cfc397c8220688e41f338ce71104a26d9f
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-12-06 13:48:47 +0700

    Display category banner

--- packages/evershop/src/modules/cms/api/admin/imageUpload/[context]multerFile[auth].js
+++ packages/evershop/src/modules/cms/api/admin/imageUpload/[context]multerFile[auth].js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 
 function fileFilter(request, file, cb) {
   // Only accept images
-  if (!/\.(jpe?g|png|gif)$/i.test(file.originalname)) {
+  if (!/\.(jpe?g|png|gif|webp)$/i.test(file.originalname)) {
     cb(null, false);
   } else {
     cb(null, true);
