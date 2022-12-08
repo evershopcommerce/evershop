5273b933e7903fd15d800639ed4dae4c3fa73425
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-12-08 09:13:22 +0700

    Add Paypal payment validation

---
+++ packages/evershop/src/modules/paypal/bootstrap.js
@@ -0,0 +1,20 @@
+const { Cart } = require('../checkout/services/cart/Cart');
+const { getSetting } = require('../setting/services/setting');
+
+module.exports = () => {
+  Cart.addField('payment_method', async function resolver(previousValue) {
+    const paymentMethod = this.dataSource?.payment_method ?? null;
+    if (paymentMethod !== 'paypal') {
+      return previousValue;
+    } else {
+      // Validate the payment method
+      const paypalStatus = await getSetting('paypalPaymentStatus');
+      if (parseInt(paypalStatus) !== 1) {
+        return previousValue;
+      } else {
+        delete this.errors['payment_method'];
+        return paymentMethod;
+      }
+    }
+  });
+}
