5247d6ee7664495c61a057a5afd408f8ca6c5b93
The Nguyen <6950941+treoden@users.noreply.github.com>
2022-12-10 09:15:40 +0700

    Update main banner styling

--- packages/evershop/src/modules/cms/pages/frontStore/homepage/MainBanner.js
+++ packages/evershop/src/modules/cms/pages/frontStore/homepage/MainBanner.js
@@ -4,10 +4,14 @@ import './MainBanner.scss';
 
 export default function MainBanner() {
   return (
-    <div className="main-banner-home flex">
-      <div className="container text-center self-center">
-        <h2 className="h1 text-white">Chic handbags reimagined for modern life.</h2>
-        <Button url="/products" title="SHOP NOW" variant="primary" />
+    <div className="main-banner-home flex items-center">
+      <div className="container grid grid-cols-1 md:grid-cols-2 gap-2">
+        <div></div>
+        <div className='text-center md:text-left px-2 '>
+          <h2 className="h1 ">Discount <span className='text-white'>20%</span> For All Orders Over $2000</h2>
+          <p>Use coupon code <span className="font-bold">DISCOUNT20</span></p>
+          <Button url="/category/women" title="SHOP NOW" variant="primary" />
+        </div>
       </div>
     </div>
   );
