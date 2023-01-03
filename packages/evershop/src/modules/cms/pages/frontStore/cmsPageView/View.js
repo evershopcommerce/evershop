a307148600bba5fc141d73a8dce86456d403e39c
The Nguyen <6950941+treoden@users.noreply.github.com>
2023-01-03 22:32:53 +0700

    Fix #162

--- packages/evershop/src/modules/cms/pages/frontStore/cmsPageView/View.js
+++ packages/evershop/src/modules/cms/pages/frontStore/cmsPageView/View.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-export default function Index({ post, category }) {
-  return <h1>This is CMS page</h1>;
+export default function Page({ page }) {
+  return <div className='page-width'>
+    <h1 className='text-center mb-3'>{page.name}</h1>
+    <div dangerouslySetInnerHTML={{ __html: page.content }}></div>
+  </div>;
 }
 
 export const layout = {
-  id: "cmsPage",
   areaId: "content",
   sortOrder: 1
 }
+
+export const query = `
+  query Query {
+    page: cmsPage(id: getContextValue("pageId")) {
+      name
+      content
+    }
+  }
+`;
