f683673e89f00341a96c907ef58092fecedaa31e
The Nguyen <6950941+treoden@users.noreply.github.com>
2023-01-25 11:12:32 +0700

    Add the default customer group

--- packages/evershop/src/modules/customer/migration/Version-1.0.1.js
+++ packages/evershop/src/modules/customer/migration/Version-1.0.1.js
@@ -9,4 +9,7 @@ module.exports = exports = async () => {
   await execute(pool, 'ALTER TABLE customer_address MODIFY uuid varchar(36) NOT NULL DEFAULT (replace(uuid(),\'-\',\'\'))');
   await execute(pool, 'ALTER TABLE customer_address ADD UNIQUE KEY `CUSTOMER_ADDRESS_UUID` (`uuid`)');
   await execute(pool, 'ALTER TABLE customer_address ADD KEY `FK_CUSTOMER` (`customer_id`)');
+
+  // Add default customer group
+  await execute(pool, 'INSERT INTO `customer_group` ( `customer_group_id`, `group_name` ) VALUES (1, `Default`) ON DUPLICATE KEY UPDATE group_name=`Default`');
 };
