bc551587b09d097234f18b179d4b5d5bce01c6b0
Cagil Seker <cagils@gmail.com>
2023-02-02 15:40:30 +0300

    Fix invalid prop in Area #115

--- packages/evershop/src/lib/components/Area.js
+++ packages/evershop/src/lib/components/Area.js
@@ -61,7 +61,7 @@ Area.propTypes = {
     id: PropTypes.string,
     sortOrder: PropTypes.number,
     component: PropTypes.shape({
-      default: PropTypes.node
+      default: PropTypes.oneOfType([PropTypes.node, PropTypes.func])
     })
   })),
   id: PropTypes.string.isRequired,
