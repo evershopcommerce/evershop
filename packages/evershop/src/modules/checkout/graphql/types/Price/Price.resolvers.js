432f483b96bfce1ed9f0837889f0696779414d30
The Nguyen <6950941+treoden@users.noreply.github.com>
2023-05-23 22:05:14 +0700

    Fix showing wrong currency issue

--- packages/evershop/src/modules/checkout/graphql/types/Price/Price.resolvers.js
+++ packages/evershop/src/modules/checkout/graphql/types/Price/Price.resolvers.js
@@ -1,16 +1,16 @@
-const { getSetting } = require('../../../../setting/services/setting');
+const { getConfig } = require('@evershop/evershop/src/lib/util/getConfig');
 
 module.exports = {
   Price: {
     value: (rawPrice) => parseFloat(rawPrice), // TODO: Format for decimal value?
     currency: async (_, { currency }) => {
-      const curr = currency || (await getSetting('storeCurrency', 'USD'));
+      const curr = currency || getConfig('shop.currency', 'USD');
       return curr;
     },
     text: async (rawPrice, { currency }) => {
       const price = parseFloat(rawPrice); // TODO: Format for decimal value?
-      const curr = currency || (await getSetting('storeCurrency', 'USD'));
-      const language = await getSetting('storeLanguage', 'en');
+      const curr = currency || getConfig('shop.currency', 'USD');
+      const language = getConfig('shop.language', 'en');
       return new Intl.NumberFormat(language, {
         style: 'currency',
         currency: curr
