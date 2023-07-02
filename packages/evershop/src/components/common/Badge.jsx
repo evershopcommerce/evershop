a435e0f0cada98f1f736837e50679ae9ca7ecee0
Chiavetta Vincenzo <vincenzo_chiavetta@icloud.com>
2023-07-02 16:26:29 +0200

    chore: refactored Badge.jsx (Enhanced performance)

--- packages/evershop/src/components/common/Badge.jsx
+++ packages/evershop/src/components/common/Badge.jsx
@@ -2,50 +2,47 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './Badge.scss';
 
+const VARIANTS = [
+  'attention',
+  'critical',
+  'default',
+  'info',
+  'new',
+  'success',
+  'warning'
+];
+
+const PROGRESSES = [
+  'complete',
+  'incomplete',
+  'partiallycomplete'
+];
+
 export default function Badge({
+  progress,
   title,
-  variant = 'default',
-  progress = 'default'
+  variant
 }) {
-  const badgeVariant = [
-    'default',
-    'success',
-    'info',
-    'attention',
-    'critical',
-    'warning',
-    'new'
-  ].includes(variant)
-    ? `${variant}`
-    : 'default';
-  const badgeProgress = [
-    'incomplete',
-    'complete',
-    'partiallycomplete'
-  ].includes(progress)
-    ? `${progress}`
-    : 'default';
+  const badgeVariant = VARIANTS.indexOf(variant) !== -1 ? variant : 'default';
+  const badgeProgress = PROGRESSES.indexOf(variant) !== -1 ? progress : 'default';
+  
   return (
     <span className={`${badgeVariant} badge`}>
       <span className={`${badgeProgress} progress rounded-100`}>
-        {progress === 'partiallycomplete' && <span />}
+        {progress === 'partiallycomplete' ? <span /> : null}
       </span>
       <span className="self-center title">{title}</span>
     </span>
   );
 }
 
+Badge.defaultProps = {
+  progress: 'default',
+  variant: 'default'
+}
+
 Badge.propTypes = {
-  progress: PropTypes.oneOf(['incomplete', 'complete', 'partiallycomplete'])
-    .isRequired,
+  progress: PropTypes.oneOf([PROGRESSES]),
   title: PropTypes.string.isRequired,
-  variant: PropTypes.oneOf([
-    'default',
-    'success',
-    'info',
-    'attention',
-    'critical',
-    'warning',
-    'new'
-  ]).isRequired
+  variant: PropTypes.oneOf(VARIANTS)
 };
