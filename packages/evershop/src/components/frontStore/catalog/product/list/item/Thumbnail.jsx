0101bbf59b41457f7d6f269e257f8565f9990282
The Nguyen <6950941+treoden@users.noreply.github.com>
2023-08-15 15:39:23 +0700

    Improve product placeholder thumbnail

--- packages/evershop/src/components/frontStore/catalog/product/list/item/Thumbnail.jsx
+++ packages/evershop/src/components/frontStore/catalog/product/list/item/Thumbnail.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import '@components/frontStore/catalog/product/list/item/Thumbnail.scss';
+import NoThumbnail from '@components/frontStore/catalog/product/single/NoThumbnail';
 
 function Thumbnail({ url, imageUrl, alt }) {
   return (
@@ -12,20 +13,7 @@ function Thumbnail({ url, imageUrl, alt }) {
       )}
       {!imageUrl && (
         <a href={url}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={0.5}
-              d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-            />
-          </svg>
+          <NoThumbnail width={100} height={100} />
         </a>
       )}
     </div>
