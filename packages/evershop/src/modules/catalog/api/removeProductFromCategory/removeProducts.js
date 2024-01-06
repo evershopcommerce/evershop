b050d1f04feb6bf99dbed4918e0cff4884e00417
The Nguyen <6950941+treoden@users.noreply.github.com>
2024-01-06 13:12:08 +0700

    Fix remove product from category

--- packages/evershop/src/modules/catalog/api/removeProductFromCategory/removeProducts.js
+++ packages/evershop/src/modules/catalog/api/removeProductFromCategory/removeProducts.js
@@ -1,30 +1,21 @@
-const {
-  getConnection
-} = require('@evershop/evershop/src/lib/postgres/connection');
+const { pool } = require('@evershop/evershop/src/lib/postgres/connection');
 const {
   INVALID_PAYLOAD,
   OK,
   INTERNAL_SERVER_ERROR
 } = require('@evershop/evershop/src/lib/util/httpStatus');
-const {
-  startTransaction,
-  rollback,
-  commit,
-  select,
-  update
-} = require('@evershop/postgres-query-builder');
+const { select } = require('@evershop/postgres-query-builder');
+const updateProduct = require('../../services/product/updateProduct');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = async (request, response, delegate, next) => {
   const { category_id, product_id } = request.params;
-  const connection = await getConnection();
-  await startTransaction(connection);
   try {
     // Check if the category is exists
     const category = await select()
       .from('category')
       .where('uuid', '=', category_id)
-      .load(connection);
+      .load(pool);
 
     if (!category) {
       response.status(INVALID_PAYLOAD);
@@ -38,7 +29,7 @@ module.exports = async (request, response, delegate, next) => {
     const product = await select()
       .from('product')
       .where('uuid', '=', product_id)
-      .load(connection);
+      .load(pool);
 
     if (!product) {
       response.status(INVALID_PAYLOAD);
@@ -49,14 +40,7 @@ module.exports = async (request, response, delegate, next) => {
     }
 
     // Remove the product from the category
-    await update('product')
-      .given({
-        category_id: null
-      })
-      .where('product_id', '=', product.product_id)
-      .execute(connection);
-
-    await commit(connection);
+    await updateProduct(product_id, { category_id: null });
     response.status(OK);
     response.json({
       success: true,
@@ -66,7 +50,6 @@ module.exports = async (request, response, delegate, next) => {
       }
     });
   } catch (e) {
-    await rollback(connection);
     response.status(INTERNAL_SERVER_ERROR);
     response.json({
       success: false,
