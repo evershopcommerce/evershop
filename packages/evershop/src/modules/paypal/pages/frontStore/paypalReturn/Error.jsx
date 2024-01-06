5a8b4b71d9ea835b03cce3a3c39d3602236c6655
Andre Westbunk <aw@npub.li>
2024-01-06 18:19:27 +0100

    Adds localization for the paypal module

--- packages/evershop/src/modules/paypal/pages/frontStore/paypalReturn/Error.jsx
+++ packages/evershop/src/modules/paypal/pages/frontStore/paypalReturn/Error.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { _ } from '@evershop/evershop/src/lib/locale/translate';
 
 export default function Error() {
   return (
     <div className="text-center">
-      <h1>Error</h1>
+       <h1>{_('Error')}</h1>
       <p>
-        We are sorry. There was an error processing your payment. Your card was
-        not charged. Please try again.
+        {_('We are sorry. There was an error processing your payment. Your card was not charged. Please try again.')}
       </p>
     </div>
   );
