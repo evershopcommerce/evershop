0d8d1b83040ca5d4c6698e6d5874e7fedf67fef4
prashant-gurung899 <prasantgrg777@gmail.com>
2024-04-03 10:02:09 +0545

    Added cucumber config file

---
+++ tests/cucumber.conf.js
@@ -0,0 +1,36 @@
+const {
+    Before,
+    BeforeAll,
+    AfterAll,
+    After,
+    setDefaultTimeout,
+  } = require("@cucumber/cucumber");
+  const { chromium } = require("playwright");
+  
+  setDefaultTimeout(60000);
+  
+  // launch the browser
+  BeforeAll(async function () {
+    global.browser = await chromium.launch({
+      headless: true,
+      slowMo: 1000,
+    });
+  });
+  
+  // Create a new browser context and page per scenario
+  Before(async function () {
+    global.context = await global.browser.newContext();
+    global.page = await global.context.newPage();
+  });
+  
+  // Cleanup after each scenario
+  After(async function () {
+    await global.page.close();
+    await global.context.close();
+  });
+  
+  // close the browser
+  AfterAll(async function () {
+    await global.browser.close();
+  });
+  
