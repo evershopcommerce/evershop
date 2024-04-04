c926ddd380704dc748728559b7ebbe0603fc4ed5
prashant-gurung899 <prasantgrg777@gmail.com>
2024-04-04 15:48:19 +0545

    Tests for delete collection completed

---
+++ tests/e2e/pageObjects/deleteCollectionPage.js
@@ -0,0 +1,17 @@
+const { expect } = require('playwright/test');
+
+class DeleteCollectionPage {
+  constructor() {
+    //locators
+    this.checkboxSelector = '//tbody/tr/td[1]';
+    this.deleteSelector = "//span[text()='Delete']";
+    this.criticalDeleteSelector = "//button[@class='button critical']";
+  }
+
+  async deleteCollection() {
+    await page.click(this.checkboxSelector);
+    await page.click(this.deleteSelector);
+    await page.click(this.criticalDeleteSelector);
+  }
+}
+module.exports = { DeleteCollectionPage };
