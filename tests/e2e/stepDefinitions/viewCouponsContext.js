ffcaea883c2a06c1c1b1e866dd856f55de3e5531
prashant-gurung899 <prasantgrg777@gmail.com>
2024-04-08 16:23:37 +0545

    view coupons context updated

--- tests/e2e/stepDefinitions/viewCouponsContext.js
+++ tests/e2e/stepDefinitions/viewCouponsContext.js
@@ -1,12 +1,12 @@
 const { When, Then } = require('@cucumber/cucumber');
 const { expect } = require('playwright/test');
-const couponsUrl = "http://localhost:3000/admin/coupons"
-const couponsTableSelector = "//table[@class='listing sticky']"
+const { ViewCouponsPage } = require('../pageObjects/ViewCouponsPage');
+const viewCouponsPage = new ViewCouponsPage();
 
 When('user {string} navigates to coupons page', async function (string) {
-  await page.goto(couponsUrl)
+  await viewCouponsPage.navigateToCouponsPage();
 });
 
 Then('user {string} should view the coupons table', async function (string) {
-  await expect(page.locator(couponsTableSelector)).toBeVisible()
+  await viewCouponsPage.checkForTable();
 });
