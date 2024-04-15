a3937a620095d5f64fa3f74b13736a7b7daf5bb3
prashant-gurung899 <prasantgrg777@gmail.com>
2024-04-15 17:09:08 +0545

    test:add e2e tests for admin login feature

---
+++ tests/e2e/stepDefinitions/adminLoginContext.js
@@ -0,0 +1,54 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+const { LoginPage } = require('../pageObjects/LoginPage');
+const login = new LoginPage();
+
+Given(
+  'user {string} has navigated to the admin login page',
+  async function (string) {
+    await login.navigateToLoginPage();
+    expect(page.url()).toBe('http://localhost:3000/admin/login');
+  }
+);
+
+When(
+  'user {string} login with following credentials',
+  async function (string, dataTable) {
+    const credentials = dataTable.hashes();
+    await login.fillLoginInputFields(credentials);
+    await login.clickLoginBtn();
+  }
+);
+
+Then(
+  'user {string} should be navigated to admin panel dashboard',
+  async function (string) {
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await expect(page.locator(login.dashboardSeletor)).toBeVisible();
+  }
+);
+
+When('the user login with following credentials', async function (dataTable) {
+  const invalidCredentials = dataTable.hashes();
+  await login.fillLoginInputFields(invalidCredentials);
+  await login.clickLoginBtn();
+});
+
+Then('error message {string} should be shown', async function (errorMessage) {
+  // await expect(page.locator(loginPage.loginBtnSelector)).toBeVisible();
+  let errorMessageSelectors = [
+    login.alertMsgSelector1,
+    login.alertMsgSelector2
+  ];
+
+  for (let selector of errorMessageSelectors) {
+    try {
+      await expect(await page.locator(selector)).toContainText(errorMessage);
+      // If the expectation passes, return immediately
+      return;
+    } catch (error) {
+      // If the expectation fails, continue to the next error message
+      continue;
+    }
+  }
+});
