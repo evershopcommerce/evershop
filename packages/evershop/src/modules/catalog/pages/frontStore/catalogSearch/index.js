e0e528671562bca81b8699445dbbde8119b932a4
The Nguyen <6950941+treoden@users.noreply.github.com>
2024-04-30 14:18:23 +0700

    Adding title to catalog search page

--- packages/evershop/src/modules/catalog/pages/frontStore/catalogSearch/index.js
+++ packages/evershop/src/modules/catalog/pages/frontStore/catalogSearch/index.js
@@ -1,4 +1,10 @@
+const {
+  translate
+} = require('@evershop/evershop/src/lib/locale/translate/translate');
 const { get } = require('@evershop/evershop/src/lib/util/get');
+const {
+  setContextValue
+} = require('@evershop/evershop/src/modules/graphql/services/contextHelper');
 
 module.exports = (request, response, delegate, next) => {
   // Get the keyword from the request query
@@ -7,6 +13,11 @@ module.exports = (request, response, delegate, next) => {
     // Redirect to the home page if no keyword is not provided
     response.redirect('/');
   } else {
+    setContextValue(request, 'pageInfo', {
+      title: translate('Search results for: ${keyword}', { keyword }),
+      description: translate('Search results for: ${keyword}', { keyword }),
+      url: request.url
+    });
     next();
   }
 };
