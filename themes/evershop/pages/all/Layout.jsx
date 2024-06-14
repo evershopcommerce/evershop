7d24861fe32f40da7b30e8556f51b5bc8ee3ba77
GenioDiferenteDND <130307809+GenioDiferenteDND@users.noreply.github.com>
2024-06-14 21:59:37 +0200

    Update Layout.jsx

--- themes/evershop/pages/all/Layout.jsx
+++ themes/evershop/pages/all/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout() {
   return (
     <>
       <LoadingBar />
-      <Banner message={<><strong>OFERTA ESPECIAL:</strong> Frete grátis em todos os pedidos acima de 5.000 MZN! Estamos em uma nova versão e ainda estamos adicionando produtos. Para ver nossa versão anterior, visite <a href="https://www.old.e-moio.com">www.old.e-moio.com</a>.</>} />
+      <Banner message={<><strong>OFERTA ESPECIAL:</strong> Frete grátis em todos os pedidos acima de 5.000 MZN! Desejamos a você Boas Compras ❤️🛍️.</>} />
 
 
       <header className="2xl:px-[50px] border-b border-header-border">
