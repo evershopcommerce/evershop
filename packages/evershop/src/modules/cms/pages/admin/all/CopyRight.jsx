de833ae210b094b690d62dc7cb3719287571f93c
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-01-02 16:24:57 +0700

    feat: Option to Change Admin Page Footer from Admin Panel #634

---
+++ packages/evershop/src/modules/cms/pages/admin/all/CopyRight.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function CopyRight({ themeConfig: { copyRight } }) {
+  return (
+    <div className="copyright">
+      <span>{copyRight}</span>
+    </div>
+  );
+}
+
+CopyRight.propTypes = {
+  themeConfig: PropTypes.shape({
+    copyRight: PropTypes.string.isRequired
+  })
+};
+
+CopyRight.defaultProps = {
+  themeConfig: {
+    copyRight: '© 2025 Evershop. All Rights Reserved.'
+  }
+};
+
+export const layout = {
+  areaId: 'footerLeft',
+  sortOrder: 10
+};
+
+export const query = `
+  query query {
+    themeConfig {
+      copyRight
+    }
+  }
+`;
--- packages/evershop/src/modules/oms/migration/Version-1.0.1.js
+++ packages/evershop/src/modules/oms/migration/Version-1.0.1.js
@@ -1,4 +1,6 @@
-const { execute } = require('@evershop/postgres-query-builder');
+const { execute, select } = require('@evershop/postgres-query-builder');
+const { warning } = require('@evershop/evershop/src/lib/log/logger');
+const { resolveOrderStatus } = require('../services/updateOrderStatus');
 
 // eslint-disable-next-line no-multi-assign
 module.exports = exports = async (connection) => {
@@ -6,4 +8,25 @@ module.exports = exports = async (connection) => {
     connection,
     `ALTER TABLE "order" ADD COLUMN IF NOT EXISTS "status" varchar DEFAULT NULL;`
   );
+
+  // Mapping the order status for legacy orders
+  const orders = await select().from('order').execute(connection);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const order of orders) {
+    try {
+      const status = resolveOrderStatus(
+        order.payment_status,
+        order.shipment_status
+      );
+      await execute(
+        connection,
+        `UPDATE "order" SET status = '${status}' WHERE order_id = ${order.order_id};`
+      );
+    } catch (err) {
+      warning(
+        `Error while updating order status for order_id: ${order.order_id}. 
+        This happened because we can not resolve the order status. You may need to update the order status manually.`
+      );
+    }
+  }
 };
