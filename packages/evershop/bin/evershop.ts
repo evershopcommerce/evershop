ff9534b0e5c37855037589f3bd22381feb118ca9
sang.tra <sang.tra@siliconprime.com>
2025-04-06 22:58:33 +0700

    Add tsx and convert to ts

--- packages/evershop/bin/evershop.ts
+++ packages/evershop/bin/evershop.ts
@@ -1,46 +1,46 @@
 #!/usr/bin/env node
-process.env.ALLOW_CONFIG_MUTATIONS = true;
+process.env.ALLOW_CONFIG_MUTATIONS = 'true';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-const { argv } = yargs(hideBin(process.argv));
+const { argv }: any = yargs(hideBin(process.argv));
 const command = argv._[0];
 try {
   if (command === 'build') {
     process.env.NODE_ENV = 'production';
-    import('./build/index.js').then((module) => module.default());
+    import('./build/index').then((module) => module.default());
   } else if (command === 'dev') {
     process.env.NODE_ENV = 'development';
-    import('./dev/index.js').then((module) => module.default());
+    import('./dev/index').then((module) => module.default());
   } else if (command === 'start') {
     process.env.NODE_ENV = 'production';
-    import('./start/index.js')
-      .then((module) => module.default())
+    import('./start/index')
+      .then((module) => module.default(() => { }))
       .catch((e) => {
         console.error(e);
       });
   } else if (command === 'install') {
-    import('./install/index.js').then((module) => module.default());
+    import('./install/index').then((module) => module.default());
   } else if (command === 'user:create') {
     import('./user/create.js').then((module) => module.default());
   } else if (command === 'user:changePassword') {
-    import('./user/changePassword.js').then((module) => module.default());
+    import('./user/changePassword').then((module) => module.default());
   } else {
     throw new Error('Invalid command');
   }
 } catch (e) {
-  import('@evershop/evershop/src/lib/log/logger.js').then((module) => {
+  import('@evershop/evershop/src/lib/log/logger').then((module) => {
     module.error(e);
   });
 }
 process.on('uncaughtException', function (exception) {
-  import('@evershop/evershop/src/lib/log/logger.js').then((module) => {
+  import('@evershop/evershop/src/lib/log/logger').then((module) => {
     module.error(exception);
   });
 });
 process.on('unhandledRejection', (reason, p) => {
   console.log('11111', p);
-  import('@evershop/evershop/src/lib/log/logger.js').then((module) => {
+  import('@evershop/evershop/src/lib/log/logger').then((module) => {
     module.error(`Unhandled Rejection: ${reason} at: ${p}`);
   });
 });
