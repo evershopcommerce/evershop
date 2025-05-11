b4f6c4e55ea1b453241e97ea5ebe1aa716dca50a
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-05-11 09:44:29 +0700

    Feat: Support typescript middleware

--- packages/evershop/src/lib/middleware/scanForMiddlewareFunctions.js
+++ packages/evershop/src/lib/middleware/scanForMiddlewareFunctions.js
@@ -3,20 +3,21 @@ import { resolve } from 'path';
 import { parseFromFile } from './parseFromFile.js';
 
 /**
-/**
  * This function take a path and scan for the middleware functions
  *
  * @param {string} path  The path of the folder where middleware functions are located
  *
  * @return {array}  List of middleware function
  */
+
 export function scanForMiddlewareFunctions(path) {
   let middlewares = [];
   readdirSync(resolve(path), { withFileTypes: true })
     .filter(
       (dirent) =>
         dirent.isFile() &&
-        /.js$/.test(dirent.name) &&
+        (/\.js$/.test(dirent.name) ||
+          (/\.ts$/.test(dirent.name) && !/\.d\.ts$/.test(dirent.name))) &&
         !/^[A-Z]/.test(dirent.name[0])
     )
     .forEach((dirent) => {
