02d9015c3825f1d23252e3c362b608aa2155be10
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-05-11 09:44:52 +0700

    Feat: Support typescript resolvers

--- packages/evershop/src/modules/graphql/services/buildResolvers.js
+++ packages/evershop/src/modules/graphql/services/buildResolvers.js
@@ -7,20 +7,22 @@ import { getEnabledExtensions } from '../../../bin/extension/index.js';
 
 export async function buildResolvers(isAdmin = false) {
   const typeSources = [
-    path.join(CONSTANTS.MOLDULESPATH, '*/graphql/types/**/*.resolvers.js')
+    path.join(CONSTANTS.MOLDULESPATH, '*/graphql/types/**/*.resolvers.{js,ts}')
   ];
 
   const extensions = getEnabledExtensions();
   extensions.forEach((extension) => {
     typeSources.push(
-      path.join(extension.path, 'graphql/types/**/*.resolvers.js')
+      path.join(extension.path, 'graphql/types/**/*.resolvers.{js,ts}')
     );
   });
 
   // Using loadFiles with an array of glob patterns instead of joining them
   const resolvers = mergeResolvers(
     await loadFiles(typeSources, {
-      ignoredExtensions: isAdmin ? [] : ['.admin.resolvers.js'],
+      ignoredExtensions: isAdmin
+        ? []
+        : ['.admin.resolvers.js', '.admin.resolvers.ts'],
       requireMethod: async (path) => {
         const module = await import(url.pathToFileURL(path));
         return module;
