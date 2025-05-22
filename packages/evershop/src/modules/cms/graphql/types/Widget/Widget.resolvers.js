90c70da10ed81d64c924fac9e1e96237e133f667
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-05-22 13:55:38 +0700

    Fix: Fix text widget error

--- packages/evershop/src/modules/cms/graphql/types/Widget/Widget.resolvers.js
+++ packages/evershop/src/modules/cms/graphql/types/Widget/Widget.resolvers.js
@@ -49,7 +49,6 @@ export default {
         : null;
     },
     textWidget(_, { text, className }) {
-      return { text: 'test', className };
       const replacements = {
         '&lt;': '<',
         '&gt;': '>'
