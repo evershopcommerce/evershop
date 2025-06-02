645e35bd748980c662efe274f09db7b1067cd6e1
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-06-02 17:59:29 +0700

    Chore: Update command to use turbo

--- packages/evershop/src/bin/dev/index.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/node-config/node-config/issues/578
-import 'dotenv/config';
-import { start } from '../../bin/lib/startUp.js';
-import { watchComponents } from '../lib/watch/watchComponents.js';
-
-export default async () => {
-  await start(watchComponents);
-};
