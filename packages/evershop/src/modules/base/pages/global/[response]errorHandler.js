6ccc092a653b47c0d701a82790a316a76611df80
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-07-10 14:02:26 +0700

    Fix: Only log error to console when debuging is enabled

--- packages/evershop/src/modules/base/pages/global/[response]errorHandler.js
+++ packages/evershop/src/modules/base/pages/global/[response]errorHandler.js
@@ -1,9 +1,12 @@
 import { encode } from 'html-entities';
 import { error } from '../../../../lib/log/logger.js';
 import { INTERNAL_SERVER_ERROR } from '../../../../lib/util/httpStatus.js';
+import isDevelopmentMode from '../../../../lib/util/isDevelopmentMode.js';
 
 export default async (err, request, response, next) => {
-  error(err);
+  if (isDevelopmentMode() || process.argv.includes('--debug')) {
+    error(err);
+  }
   // Set this flag to make sure this middleware only be executed 1 time
   response.locals.errorHandlerTriggered = true;
   // Check if the header is already sent or not.
--- packages/evershop/src/types/response.ts
+++ packages/evershop/src/types/response.ts
@@ -2,4 +2,5 @@ import { Response as ExpressResponse } from 'express';
 
 export interface EvershopResponse extends ExpressResponse {
   debugMiddlewares: { id: string; time: number }[];
+  $body: Record<string, unknown>;
 }
