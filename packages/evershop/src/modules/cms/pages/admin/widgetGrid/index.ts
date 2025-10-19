ce83af7c40d6fae9608dcde26341afde53413b6e
The Nguyen <6950941+treoden@users.noreply.github.com>
2025-10-19 15:01:33 +0700

    Fix: Fix filter not working admin grid

--- packages/evershop/src/modules/cms/pages/admin/widgetGrid/index.ts
+++ packages/evershop/src/modules/cms/pages/admin/widgetGrid/index.ts
@@ -1,11 +1,16 @@
 import { buildFilterFromUrl } from '../../../../../lib/util/buildFilterFromUrl.js';
+import { EvershopRequest } from '../../../../../types/request.js';
 import { setContextValue } from '../../../../graphql/services/contextHelper.js';
 import { setPageMetaInfo } from '../../../services/pageMetaInfo.js';
 
-export default (request, response) => {
+export default (request: EvershopRequest, response) => {
   setPageMetaInfo(request, {
     title: 'Widgets',
     description: 'Widgets'
   });
-  setContextValue(request, 'filtersFromUrl', buildFilterFromUrl(request));
+  setContextValue(
+    request,
+    'filtersFromUrl',
+    buildFilterFromUrl(request.originalUrl)
+  );
 };
